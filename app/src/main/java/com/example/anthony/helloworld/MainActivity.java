fbbacbefd6a74ce27bb8c595d920f2aa8d3e36ea
Anthony <anthonytejad21@gmail.com>
2019-02-04 21:26:10 -0500

    Toast added

--- app/src/main/java/com/example/anthony/helloworld/MainActivity.java
+++ app/src/main/java/com/example/anthony/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.anthony.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
+
 }
